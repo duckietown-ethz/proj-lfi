f01b5bf4c80adf9804cdb217b9734b1b1719ee95
wlan-m <matthias.wieland@hotmail.com>
2019-12-04 11:49:09 +0100

    Update trajectory following node

--- code/arc_trajectory/trajectory_following.py
+++ code/arc_trajectory/trajectory_following.py
@@ -7,12 +7,12 @@ import rospy
 import yaml
 
 from duckietown import DTROS
-from sensor_msgs.msg import CompressedImage, Image
-from duckietown_msgs.msg import WheelsCmdStamped
+from duckietown_msgs.msg import WheelsCmdStamped, LanePose
 from cv_bridge import CvBridge
 from duckietown_utils import load_map, load_camera_intrinsics, load_homography, rectify
 from collections import OrderedDict
 from numpy import linalg as LA
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
 
 class TrajectoryControl(DTROS):
@@ -29,26 +29,70 @@ class TrajectoryControl(DTROS):
         y_rate = self.rospy.get_param('/yRate')
         tangent_angle = self.rospy.get_param('/tangentAngle')
 
-        # self.pub_image_ar = rospy.Publisher("~/%s/augmented_reality_node/%s/image/compressed" % (name, imageName), CompressedImage, queue_size=10)
         self.pub_image_ar = rospy.Publisher("~/.../lane_pose" % (name, imageName), LanePose, queue_size=1)
 
-        # print("wtf!!!")
-
-        # self.sub_cam_img = rospy.Subscriber("~/%s/camera_node/image/compressed" % name, CompressedImage, self.callback, queue_size=1)
         self.sub_cam_img = rospy.Subscriber("~/.../pose_stamped" % name, PoseStamped, self.callback, queue_size=1)
         self.log("Initialized")
 
 
     def callback(self, msg):
-        d, phi = self.run_pid(msg)
+        '''
 
-        # image_ar_msg = Augmented.process_image(self, msg)
-        self.pub_image_ar.publish(msg_pose_stamped)
+        :param msg:
+        :return:
+        '''
+        # compte motor commands for lefta nd right motor with a simple line following pid controller
+        # --> this function is INCOMPLETE!
+        # u_l, u_r = self.run_pid(msg)
+
+        # compute distance d and angle phi, same as in line following
+        d, phi = self.relative_pose(msg)
+
+        # convert to LanePose() message
+        msg_pub = LanePose()
+        msg_pub.header.stamp = ...
+        msg_pub.d = d
+        msg_pub.phi = phi
+        msg_pub.in_lane = ...
+        # TODO: is it always NORMAL?
+        lanePose.status = lanePose.NORMAL
+
+
+        # publish lane pose msg
+        self.pub_image_ar.publish(msg_lane_pose)
+
+
+    def relative_pose(self, msg):
+        '''
+        This function uses the current position of the duckiebot in the intersection-stop-line frame to find the closest
+        distance to the optimal trajectory and the angle difference between the closest point of the optimal trajectory
+        and current orientation of the duckiebot.
+        :param msg: current position of the duckiebot in the intersection-stop-line frame
+        :return: distance d and angle phi compared to the optimal trajectory --> same as for line following
+        '''
+
+        db_position = msg.pose.pose.position
+        db_orientation = msg.pose.pose.orientation
+        orientation_list = [db_orientation.x, db_orientation.y, db_orientation.z, db_orientation.w]
+        (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
+
+        idx_min_dist, min_dist = self.closest_track_point(db_position.x, db_position.y)
+        side = self.track_side(track_pt1, track_pt2, car_pt)
+
+        closest_pt = np.array([x_track[idx_min_dist], y_track[idx_min_dist]])
+        closest_angle = tangent_angle[idx_min_dist]
+
+        d = np.norm(closest_pt)*side
+        phi = abs(closest_angle - yaw)*side
+
+        return d, phi
 
 
     def run_pid(self, msg):
         '''
         PID controller which follows the optimal trajectory generated offline and stored in yaml files
+        --> This function is incomplete: some parameters need to be defined, the velocity needs to be computed and a
+        mapping from steer to torque left and right
         :param msg: PoseStamed() message: current duckiebot position and orientation in stopline frame ?
         :return: motor commands
         '''
@@ -67,7 +111,17 @@ class TrajectoryControl(DTROS):
         position_error = side * min_dist
         integral_error += position_error
 
-        # TODO: finish PID on line 185
+        u_steer = kp*position_error + kd*(position_error-prev_position_error) + ki*integral_error
+        prev_position_error = position_error
+
+        u_torque = target_velocity
+
+        # TODO: find mapping from (torque, steer) to (torque left, torque right)
+        torque_left = fcn(u_torque, u_steer)
+        torque_right = fcn(u_torque, u_steer)
+
+        return torque_left, torque_right
+
 
 
 
